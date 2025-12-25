a1ed7553f47855a376f6b7c0125e08224bd10d81
RauTheLegendary <rauthelegendary@gmail.com>
2025-12-25 12:59:05 +0100

    Add files via upload

---
+++ Files/scripts/game-select/game-select.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo -n "${1}" > /sdcard/Tasker/esde_game_scroll.txt
+echo -n "${2}" > /sdcard/Tasker/esde_game.txt
+echo -n "${4}" > /sdcard/Tasker/esde_system.txt
+echo -n " " > /sdcard/Tasker/esde_default.txt
